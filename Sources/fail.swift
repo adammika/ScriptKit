7a987cfaa226479032735c84719cebcbcebc54d1
Adam Mika <mika.adam@gmail.com>
2017-04-16 11:24:59 -0500

    Fail never returns

--- Sources/fail.swift
+++ Sources/fail.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public func fail(_ message: String, code: Int32 = 1) {
+public func fail(_ message: String, code: Int32 = 1) -> Never {
   print("Error: \(message)")
   exit(code)
 }
